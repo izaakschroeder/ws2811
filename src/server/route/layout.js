e9cf4882d3349001dc43df8ac8ab807a0289f43a
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-12-20 14:54:48 -0800

    lol. remove chans.

--- src/server/route/layout.js
+++ src/server/route/layout.js
@@ -16,24 +16,46 @@ routes.post('/', (req, res) => {
   res.status(200).send(set(req.body));
 });
 
+/**
+ * Fetch the matching pin for any pin in a request. e.g. Fetch data for P9_22
+ * in the request `/P9_22/state`. If the pin is not found, raise an error.
+ */
 routes.param('pin', (req, res, next, id) => {
   const layout = get();
-  req.pin = layout[id];
-  next();
+  if (id in layout) {
+    req.pin = layout[id];
+    next();
+  } else {
+    next({ status: 404 });
+  }
 });
 
+/**
+ *
+ */
 routes.patch('/:pin/state', (req, res) => {
   res.send(500);
 });
 
-routes.put('/:pin/state', (req, res) => {
+/**
+ * The simplest update operation. Overwrite all pixel values for a pin. For
+ * more complex/nuanced operations use the `PATCH` method.
+ */
+routes.put('/:pin/state', (req, res, next) => {
   const pin = req.pin;
-  for (let i = 0; i < req.body.length; ++i) {
-    pin.data.writeUInt32LE(req.body[i], i * 4);
+  if (req.body.length !== pin.data.length) {
+    next({ status: 400, error: 'INVALID_LENGTH', expected: pin.data.length });
+  } else {
+    for (let i = 0; i < req.body.length; ++i) {
+      pin.data.writeUInt32LE(req.body[i], i * 4);
+    }
+    res.send(200);
   }
-  res.send(200);
 });
 
+/**
+ * Fetch the current pixel data being fed to a pin.
+ */
 routes.get('/:pin/state', (req, res) => {
   res.send(req.pin.data);
 });
